c249cb158a5c0a49f65756173cf066e243e9c8ee
Raphe <raph0713@gmail.com>
2024-10-01 15:51:00 -0400

    wip board card

--- frontend/src/features/Boards/components/BoardCard.vue
+++ frontend/src/features/Boards/components/BoardCard.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <q-card class="board-card">
+    <img
+      src="https://cdn.quasar.dev/img/mountains.jpg"
+      class="board-card__img"
+    />
+    <q-card-section class="q-pa-sm row justify-between items-center">
+      <div>created at</div>
+      <q-btn
+        round
+        padding="xs"
+        color="red"
+        icon="delete"
+      />
+    </q-card-section>
+  </q-card>
+</template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.board-card {
+  max-width: 256px;
+  height: 256px;
+
+  &__img {
+    object-fit: cover;
+    height: 80%;
+  }
+}
+</style>
--- frontend/src/features/Boards/components/BoardsIndex.vue
+++ frontend/src/features/Boards/components/BoardsIndex.vue
@@ -1,34 +1,67 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { computed, reactive, ref } from "vue";
 
-const foo = reactive({
-  abc: {
-    name: "foo",
-  },
-  def: {
-    name: "bar",
-  },
+import store from "@/modules/store";
+
+import BoardCard from "./BoardCard.vue";
+
+const boards = computed(() => {
+  return store.state.boards;
 });
+
+const addBoardOpen = ref(false);
+
+const openAddBoard = () => {
+  console.log("click");
+  addBoardOpen.value = true;
+};
 </script>
 
 <template>
   <q-page
     padding
-    class="flex flex-center text-center"
+    class="flex flex-start text-center"
+    @click="addBoardOpen = false"
   >
-    <div class="row q-col-gutter-md">
-      <div
-        v-for="(user, key) in foo"
+    <div class="row q-gutter-md">
+      <board-card
+        v-for="(board, key) in boards"
         :key="key"
+        :board="board"
         class="col"
+      />
+
+      <q-card
+        flat
+        class="add-board flex flex-center justify-center"
+        :class="{ '-active': addBoardOpen }"
+        @click.stop="openAddBoard"
       >
-        <q-card>
-          <q-card-section> {{ user.name }}</q-card-section>
-          <q-card-actions>
-            <q-btn> Action </q-btn>
-          </q-card-actions>
-        </q-card>
-      </div>
+        <q-card-section class="no-padding"> Add a board... </q-card-section>
+      </q-card>
     </div>
   </q-page>
 </template>
+
+<style scoped lang="scss">
+.add-board {
+  max-height: 2rem;
+  min-height: 2rem;
+  padding: 0.5rem 1rem;
+  background-color: $grey-6;
+  color: white;
+
+  transition: min-height 0.2s ease;
+
+  &:hover {
+    cursor: pointer;
+    background-color: $grey-7;
+  }
+
+  &.-active {
+    background-color: $grey-2;
+    color: black;
+    min-height: 10rem;
+  }
+}
+</style>
