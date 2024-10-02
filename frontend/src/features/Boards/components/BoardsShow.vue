22c012a500c727cb147df7221bab5d8e99f0ab85
Raphe <raph0713@gmail.com>
2024-10-02 10:40:03 -0400

    board show view and card

--- frontend/src/features/Boards/components/BoardsShow.vue
+++ frontend/src/features/Boards/components/BoardsShow.vue
@@ -1,13 +1,118 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { uid } from "uid";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+import store from "@/modules/store";
+
+import TaskList from "./TaskList.vue";
+
+const route = useRoute();
+
+const id = route.params.id;
+
+const board = store.state.boards[id];
+
+const addListOpen = ref(false);
+const openAddList = () => {
+  addListOpen.value = true;
+};
+
+const newListName = ref("");
+
+const createNewList = () => {
+  const id = uid();
+  board.taskLists[id] = {
+    id: id,
+    name: newListName.value,
+  };
+  newListName.value = "";
+};
+</script>
 
 <template>
   <q-page
-    padding
-    class="flex flex-start text-center"
-    @click="addBoardOpen = false"
+    class="column flex-start text-center"
+    @click="addListOpen = false"
   >
-    Board !
+    <div
+      class="bar q-pa-md cursor-pointer"
+      @click="$router.push({ name: 'boards-index' })"
+    >
+      {{ board.name }}
+    </div>
+
+    <div class="row q-gutter-md q-pa-md">
+      <task-list
+        v-for="(list, key) in board.taskLists"
+        :key="key"
+        :list="list"
+        :board="board"
+      />
+
+      <q-card
+        flat
+        class="add-list flex flex-center justify-center"
+        :class="{ '-active': addListOpen }"
+        @click.stop="openAddList"
+      >
+        <q-card-section class="no-padding">
+          <template v-if="!addListOpen">Add a list...</template>
+          <template v-else>
+            <q-form
+              class="q-gutter-md column"
+              @submit="createNewList"
+            >
+              <q-input
+                v-model="newListName"
+                required
+                label="List name"
+                filled
+              />
+              <q-btn
+                type="submit"
+                color="primary"
+                size="md"
+              >
+                Submit
+              </q-btn>
+            </q-form>
+          </template>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.bar {
+  height: 3rem;
+  background-color: $grey-6;
+  display: flex;
+  align-items: center;
+  color: white;
+  font-size: 1.25rem;
+}
+
+.add-list {
+  max-height: 2rem;
+  min-height: 2rem;
+  padding: 0.5rem 1rem;
+  background-color: $grey-6;
+  color: white;
+  overflow: hidden;
+  transition: all 0.2s ease;
+  width: 234px;
+
+  &:hover {
+    cursor: pointer;
+    background-color: $grey-7;
+  }
+
+  &.-active {
+    background-color: $grey-2;
+    color: black;
+    min-height: 10rem;
+  }
+}
+</style>
---
+++ frontend/src/features/Boards/components/TaskList.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps(["board", "list"]);
+</script>
+
+<template>
+  <q-card class="tasklist">
+    <q-card-section>
+      <div class="text-subtitle1 q">{{ props.list.name }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<style scoped lang="scss">
+.tasklist {
+  min-height: 20rem;
+  min-width: 10rem;
+}
+</style>
